c9557a0d8c5dab7e1c23d3033a30ea5c49bccc84
Saimun <saimun4u@gmail.com>
2021-06-17 17:16:34 -0500

    JS element selected and setting up logic

---
+++ app.js
@@ -0,0 +1,14 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const form = document.getElementById('form');
+const list = document.getElementById('list');
+const form = document.getElementById('balance');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+    { Id : 1, text: 'Flower', amount: -20},
+    { Id : 2, text: 'Salary', amount: 500},
+    { Id : 3, text: 'Book', amount: -20},
+    { Id : 4, text: 'Camera', amount: -120}
+]
