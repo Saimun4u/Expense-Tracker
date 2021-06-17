57bd4d846f5e482a157f2a8ea25a8420eb0e39fe
Saimun <saimun4u@gmail.com>
2021-06-17 17:37:07 -0500

    JS Logic for history item setup

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
-const form = document.getElementById('form');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
-const form = document.getElementById('balance');
+const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
@@ -12,3 +12,34 @@ const dummyTransactions = [
     { Id : 3, text: 'Book', amount: -20},
     { Id : 4, text: 'Camera', amount: -120}
 ]
+
+let transactions = dummyTransactions;
+
+//Add transactions to DOM list
+
+function addTransactionDOM(transaction){
+
+    //Get sign
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+
+    //Add clase based on value
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn"></button>
+    `;
+
+    list.appendChild(item);
+}
+
+//Init app
+
+function init(){
+    list.innerHTML = ' ';
+
+    transactions.forEach(addTransactionDOM);
+}
+
+init()
