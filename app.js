b7256afa109c2ba30613193287195a62801b00be
Saimun <saimun4u@gmail.com>
2021-06-18 19:54:48 -0500

    Logic for calculation of total balance, income and expense set up

--- app.js
+++ app.js
@@ -37,9 +37,15 @@ function addTransactionDOM(transaction){
 //Update Values
 
 function updateValues(){
-    const amounts = transactions.forEach(transaction => {
-        console.log(transaction.amount);
-    })
+   const amounts = transactions.map((transaction) => (transaction.amount));
+
+   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+   
+   const income = amounts.filter((item) => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+   
+   const expense = amounts.filter((item) => item < 0).reduce((acc, item) => (acc -= item), 0).toFixed(2) * -1;
+   console.log(expense)
+
 }
 
 //Init app
